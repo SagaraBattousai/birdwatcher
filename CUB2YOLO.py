db670f3568cccbc78788bdf67852275db7800d33
James Calo <jamesafcalo@gmail.com>
2025-06-14 22:52:22 +0100

    Added functions for average size and images uner a certain size. Gonna resize to 224, should refactor code to pass lambda as too much duplication, better check closures work safely in python

--- CUB2YOLO.py
+++ CUB2YOLO.py
@@ -29,8 +29,8 @@ def getMinImageDimensions(
     minWidth = (65535, "")
     minHeight = (65535, "")
 
-    for dirpath, dirnames, filenames in os.walk(images_root_dir):
-        #As is the case of the root dir and theoretically any empty dirs
+    for dirpath, _, filenames in os.walk(images_root_dir):
+        # As is the case of the root dir and theoretically any empty dirs
         if len(filenames) == 0:
             continue
         for imagefile in filenames:
@@ -45,10 +45,70 @@ def getMinImageDimensions(
     return (minWidth, minHeight)
 
 
+# ANSWER = (467.88683406854426, 386.02994570749917)
+def getAvgImageDimensions(images_root_dir: str) -> tuple[float, float]:
+    total_width = 0
+    total_height = 0
+    image_count = 0
+
+    for dirpath, _, filenames in os.walk(images_root_dir):
+        # As is the case of the root dir and theoretically any empty dirs
+        if len(filenames) == 0:
+            continue
+        for imagefile in filenames:
+            with Image.open(f"{dirpath}/{imagefile}") as image:
+                total_width += image.width
+                total_height += image.height
+                image_count += 1
+
+    return (total_width / image_count, total_height / image_count)
+
+
+# under 128 = (1, 1, 0)
+# under 256 = (152, 241, 102) # Thats 291 total images
+#
+# under 224 = (72, 125, 49) # 148 total *** This is the one!
+# under 448 = (2571, 9001, 675) # 10897 total
+#
+def get_num_images_under_size(
+    images_root_dir: str, min_width: int = 256, min_height: int = 256
+) -> tuple[int, int, int]:
+    under_width_count = 0
+    under_height_count = 0
+    under_both_count = 0
+
+    for dirpath, _, filenames in os.walk(images_root_dir):
+        # As is the case of the root dir and theoretically any empty dirs
+        if len(filenames) == 0:
+            continue
+        for imagefile in filenames:
+            with Image.open(f"{dirpath}/{imagefile}") as image:
+                if image.width < min_width:
+                    under_width_count += 1
+                    if image.height < min_height:
+                        under_height_count += 1
+                        under_both_count += 1
+                        # could write continue but elif does that :)
+                elif image.height < min_height:
+                    under_height_count += 1
+
+    return (under_width_count, under_height_count, under_both_count)
+
+
 if __name__ == "__main__":
     classmap = getClassMap(DS_ROOT + "/classes.txt")
     print(classmap)
 
-    minDims = getMinImageDimensions(DS_ROOT + "/images")
-    print(minDims)
+    # minDims = getMinImageDimensions(DS_ROOT + "/images")
+    # print(minDims)
+
+    # avgDims = getAvgImageDimensions(DS_ROOT + "/images")
+    # print(avgDims)
+
+
+    under_448 = get_num_images_under_size(DS_ROOT + "/images", 448, 448)
+    print(under_448)
+
+
+
     sys.exit(0)
